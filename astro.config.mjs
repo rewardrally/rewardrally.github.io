fa2be29a50f066896834343427885d23c645dc13
Saravanapriyan <saravanapriyanm@gmail.com>
2026-03-03 17:26:35 +0530

    init: docs

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Reward Rally Documentation",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Shopify",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Install", slug: "shopify/install" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
